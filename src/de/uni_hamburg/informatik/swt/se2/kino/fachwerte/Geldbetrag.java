821451dd9a451a6be95cc88ea999ce11fc867e2b
enegatiph <enegatiph@googlemail.com>
2012-07-03 13:49:52 +0200

    praesenzuebung fertig, todos in GEldbetragTest.java

--- src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/Geldbetrag.java
+++ src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/Geldbetrag.java
@@ -3,6 +3,7 @@ package de.uni_hamburg.informatik.swt.se2.kino.fachwerte;
 public class Geldbetrag {
 
 	public Geldbetrag() {
+		final int _preis; 
 		// TODO Auto-generated constructor stub
 	}
 
--- src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/GeldbetragTest.java
+++ src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/GeldbetragTest.java
@@ -9,12 +9,19 @@ import org.junit.Test;
 
 public class GeldbetragTest {
 
-
+// Material Vorstellung arbeitet mit Geldbetrag
+// regex: (-?\\d{1,7})(,(\\d{0,2}))?
+// wenn oder {1,8} und wenn die zahl zu gross ist gleich Integer.MAX_VALUE setzen
+// methode istgueltig
+	
+	
+	
+	
 	@Test
 	void testeCompareTo()
 	{
-		Geldbetrag g1 = new Geldbetrag(1500);
-		Geldbetrag g2 = new Geldbetrag(2000);
+		Geldbetrag g1 = new Geldbetrag(Integer.MIN_VALUE);
+		Geldbetrag g2 = new Geldbetrag(Integer.MAX_VALUE);
         assertTrue(g1.compareTo(g2) < 0);
         assertTrue(g2.compareTo(g1) > 0);
         assertTrue(g1.compareTo(g1) == 0);
@@ -25,9 +32,7 @@ public class GeldbetragTest {
 	{
 		Geldbetrag g1 = new Geldbetrag(1500);
 		Geldbetrag g2 = new Geldbetrag(2000);
-        assertEquals( 3500, g1.add(g2));
-        assertEquals(3000, g2.add(1000));
-        assertEquals(51500, g1.add("500,00"));
+        assertEquals( new Geldbetrag(3500), g1.add(g2));
 	}
 	
 	@Test
@@ -35,16 +40,16 @@ public class GeldbetragTest {
 	{
 		Geldbetrag g1 = new Geldbetrag(1500);
 		Geldbetrag g2 = new Geldbetrag(2000);
-        assertEquals( 500, g2.sub(g1));
-        assertEquals( 500, g1.sub(1000));
-        assertEquals( -100, g1.sub("16,00"));
+        assertEquals( new Geldbetrag(500), g2.sub(g1));
+        assertEquals( new Geldbetrag(500), g1.sub(1000));
+        assertEquals( new Geldbetrag(-100), g1.sub("16,00"));
 	}
 
 	@Test
 	void testeMul()
 	{
 		Geldbetrag g1 = new Geldbetrag(1500);
-        assertEquals( 7500, g1.mul(5));
+        assertEquals( new Geldbetrag(7500), g1.mul(5));
 	}
 
 	@Test
@@ -52,24 +57,14 @@ public class GeldbetragTest {
 	{
 		Geldbetrag g1 = new Geldbetrag(1500);
         assertEquals( "15,00", g1.toString());
+        // 99c, 7cent, 0cent, randfaelle halt, negative werte, 008,3 eur, 
 	}
 
 	@Test
-	void testeToInt()
+	void testeToInt() // rm
 	{
 		Geldbetrag g1 = new Geldbetrag(1500);
         assertEquals( 1500, g1.toInt());
-	}
-
-	@Test
-	void testeSet()
-	{
-		Geldbetrag g1 = new Geldbetrag(1500);
-        assertEquals( 1500, g1.toInt());
-        g1.Set(5000);
-        assertEquals( 5000, g1.toInt());
-        g1.Set("20,12");
-        assertEquals( 2012, g1.toInt());
 	}
 
 	@Test
@@ -78,26 +73,24 @@ public class GeldbetragTest {
 		Geldbetrag g1 = new Geldbetrag(1500);
 		Geldbetrag g2 = new Geldbetrag("200,00");
 		Geldbetrag g3 = new Geldbetrag("700");
-		assertEquals( 1500, g1.toInt());
-		assertEquals( 20000, g1.toInt());
-		assertEquals( 70000, g1.toInt());
+		assertEquals( new Geldbetrag(1500), g1.toInt());
+		assertEquals( new Geldbetrag(20000), g1.toInt());
+		assertEquals( new Geldbetrag(70000), g1.toInt());
+		// test mit 1, 2, und 0 kommastellen
 	}
 	
-	@Test
-	void testeIntegerObject()
+	@Test(expected=IllegalArgumentException.class)
+	void testeKonstruktorParse()
 	{
-		Geldbetrag g1 = new Geldbetrag(new Integer(1500));
-		assertEquals( 1500, g1.toInt());
+		Geldbetrag g1 = new Geldbetrag("Peter");
 	}
-
 	
 	@Test
 	void testeAntiRoundhouseKick()
 	{
 		Geldbetrag g1 = new Geldbetrag("Chuck Norris");
-        assertEquals( "infinity" , g1.toString());
+		assertEquals( "infinity" , g1.toString());
         assertEquals( Integer.MAX_VALUE , g1.toInt());
 	}
-
 
 }
