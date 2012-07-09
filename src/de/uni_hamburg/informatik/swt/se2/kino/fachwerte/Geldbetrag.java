d136103b241662a58f68df2089896daa59300d55
enegatiph <enegatiph@googlemail.com>
2012-07-09 17:04:34 +0200

    Konstruktoren fuer Geldbetrag implementiert, ueberladung fuer string weggelassen aufgrund von Geldbetrag.parse(String)

--- src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/Geldbetrag.java
+++ src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/Geldbetrag.java
@@ -1,10 +1,33 @@
 package de.uni_hamburg.informatik.swt.se2.kino.fachwerte;
 
-public class Geldbetrag {
+/**
+ * Ein Geldbetrag wird als Centwert gespeichert (Waehrung ist Euro)
+ * Das Klassenobjekt stellt eine Hilfsmethode zur Verfügung um zu prüfen 
+ * ob es sich bei einer Zeichenkette um eine gültige Repräsentation eines 
+ * Geldbetrages handelt.
+ */
+public final class Geldbetrag implements Comparable<Geldbetrag>
+{
+	final int _betrag;
 
-	public Geldbetrag() {
-		final int _preis; 
-		// TODO Auto-generated constructor stub
+	/**
+	 * @ensure:
+	 *   new Geldbetrag(0).equals(this) == true
+	 */
+	public Geldbetrag() 
+	{
+		_betrag = 0; 
 	}
-
+	
+	/**
+	 * @param betrag
+	 *   Der Geldbetrag in (Euro-)Cent
+	 * @ensure:
+	 *   new Geldbetrag(betrag).equals(this) == true
+	 */
+	public Geldbetrag(int betrag)
+	{
+		_betrag = betrag;
+	}
+	
 }
